c7c89ed298272d1395ae315e5a64463c359cbed2
Karolos Giampouonka-Kanellakos <carolosjk@hotmail.com>
2025-10-20 19:55:38 +0300

    1st problem

---
+++ Arrays & Hashing/Contains Duplicate.py
@@ -0,0 +1,12 @@
+
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        numbers_we_found = {}
+        for i in nums:
+            if i in numbers_we_found:
+                return True
+            numbers_we_found[i] = True
+
+        return False
