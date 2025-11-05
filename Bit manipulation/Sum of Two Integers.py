ed0bb8e895f5f1dff65e2ab67916945ba36c7b4c
Karolos Giampouonka-Kanellakos <carolosjk@hotmail.com>
2025-11-06 01:05:59 +0200

    sum of 2 numbers

---
+++ Bit manipulation/Sum of Two Integers.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        
+        while b !=0:
+            carry = (a&b) << 1
+            a = a^b & mask
+            b = carry & mask
+        
+        if a > 0x8FFFFFFF:
+            a = ~(a ^ mask)
+        return a
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.getSum(1,2))
+    print(solution.getSum(2,-5))
+    print(solution.getSum(10,-3))
